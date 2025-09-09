e4cf1ac99b26c416140349fa5b7e11c788f628fa
ericssendelima <ericssendelima@gmail.com>
2025-09-09 09:55:00 -0300

    feat: Cria as props da classe worker

--- Composicoes-Enums/WorkerContracts/Entities/Worker.cs
+++ Composicoes-Enums/WorkerContracts/Entities/Worker.cs
@@ -1,8 +1,11 @@
+using WorkerContracts.Enums;
+
 namespace WorkerContracts.Entities
 {
-  public class Worker(string name)
+  public class Worker(string name, WorkLevel level, double salary)
   {
     private readonly string _name = name;
-    
-    }
+    private readonly WorkLevel _level = level;
+    private readonly double _salary = salary;
+  }
 }
